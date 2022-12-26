712a342384b8868a4a7b3467f5db136d7edc492f
Anar Abbasov <anariuos@gmail.com>
2022-12-26 21:33:12 +0400

    first Pascals' Triangle

---
+++ Stepik/Pascal's Triangle.py
@@ -0,0 +1,17 @@
+def pascal(n):
+
+    list1 = []
+    for i in range(n+1):
+        temp_list = []
+        for j in range(i+1):
+            if j ==0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(list1[i-1][j-1] + list1[i-1][j])
+        list1.append(temp_list)
+
+    return list1[n]
+
+
+n = int(input('Enter the row number: '))
+print(pascal(n))
