bc93c38271f0b8e20d469c2b8c70cceba932dca6
Anar Abbasov <anariuos@gmail.com>
2023-01-02 14:21:59 +0400

    edited sum

--- Stepik/Sum Matrix.py
+++ Stepik/Sum Matrix.py
@@ -2,9 +2,13 @@ n, m = [int(i) for i in input().split()]
 
 matrix1 = [[int(i) for i in input().split(' ')] for _ in range(n)]
 input()
-matrix2 = [[int(i) for i in input().split(' ')] for _ in range(n)]
+l, k = [int(i) for i in input().split()]
+matrix2 = [[int(i) for i in input().split(' ')] for _ in range(l)]
+matrix3 = [[0 for _ in range(k)] for _ in range(n)]
 
-for r in range(n):
-    for c in range(m):
-        print(matrix1[r][c] + matrix2[r][c], end=' ')
+for x in range(n):
+    for r in range(k):
+        for c in range(l):
+            matrix3[x][r] += matrix1[x][c] * matrix2[c][r]
+        print(matrix3[x][r], end=' ')
     print()
--- Stepik/matrix by matrix.py
+++ Stepik/matrix by matrix.py
@@ -1,14 +1,18 @@
-n, m = [int(i) for i in input().split()]
+n = int(input())
 
 matrix1 = [[int(i) for i in input().split(' ')] for _ in range(n)]
-input()
-l, k = [int(i) for i in input().split()]
-matrix2 = [[int(i) for i in input().split(' ')] for _ in range(l)]
-matrix3 = [[0 for _ in range(k)] for _ in range(n)]
 
-for x in range(n):
-    for r in range(k):
-        for c in range(l):
-            matrix3[x][r] += matrix1[x][c] * matrix2[c][r]
-        print(matrix3[x][r], end=' ')
-    print()
+m = int(input())
+
+matrix2 = [[0 for _ in range(n)] for _ in range(n)]
+
+for b in range(m):
+    for x in range(n):
+        for r in range(n):
+            for c in range(n):
+                matrix2[x][r] += matrix1[x][c] * matrix2[c][r]
+            print(matrix2[x][r])
+        print()
+
+
+
